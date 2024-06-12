1b53719c7b9152b7eaed50a7887ec26caf98823d
SameepSig <sameepsigdel@gmail.com>
2024-06-12 13:20:21 +0545

    getenv returning blank solved

--- app/config.php
+++ app/config.php
@@ -5,7 +5,8 @@ require __DIR__ . '/../vendor/autoload.php';
 use Dotenv\Dotenv;
 
 // Load environment variables from .env file in the root directory
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+// $dotenv = Dotenv::createUnImmutable(__DIR__ . '/../');
+$dotenv = Dotenv::createUnSafeImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 /* Database credentials from environment variables */
@@ -14,10 +15,21 @@ define('DB_USERNAME', getenv('DB_USERNAME'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_NAME', getenv('DB_NAME'));
 
-print_r(getenv('DB_SERVER'));
-print_r(getenv('DB_USERNAME'));
-print_r(getenv('DB_PASSWORD'));
-print_r(getenv('DB_NAME'));
+//define('Greetings', "K xa");
+//define('DB_SERVER', 'localhost');
+//define('DB_USERNAME', 'test_user');
+//define('DB_PASSWORD', 'test_0000');
+//define('DB_NAME', 'test_db');
+
+
+//print_r("Hello");
+//print_r(Greetings);
+// print_r(DB_SERVER);
+
+// print_r(getenv('DB_SERVER'));
+// print_r(getenv('DB_USERNAME'));
+// print_r(getenv('DB_PASSWORD'));
+// print_r(getenv('DB_NAME'));
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
